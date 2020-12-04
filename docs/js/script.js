ef2b440d41fb44d34f43f7128b9aba7a3c0d911f
Andrii Valchuk <andriy.valchuk@gmail.com>
2020-12-04 20:05:18 +0200

    add cell position

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,6 +1,15 @@
 $( document ).ready(function() {
+	const cellSize = 100;
+	const empty = {
+		top: 0,
+		left: 0
+	};
+
 	for (let i = 1; i <= 15; i++) {
-		const cell = $(document.createElement(`div`)).addClass("cell border border-primary rounded").append(i);
+		const left = (i % 4);
+		const top = ((i - left) / 4);
+		const cell = $(document.createElement('div')).addClass("cell border border-primary rounded position-absolute").append(i).css("left", left*cellSize).css("top", top*cellSize);
+
 		$('#field').append(cell);
 	}
 });
